2ef23e3c29a0924ce0eb9b0f5ed3bd6a49e42dba
No1_NrC7 <nobodyoffc@gmail.com>
2024-01-01 11:50:59 +0800

    create

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,10 +2,12 @@ import org.bitcoinj.core.Address;
 import org.bitcoinj.core.ECKey;
 import org.bitcoinj.params.MainNetParams;
 import org.bitcoinj.script.Script;
+import params.DogecoinMainNetParams;
 
 public class Test {
     public static void main(String[] args) {
         ECKey ecKey = new ECKey();
         System.out.println(Address.fromKey(MainNetParams.get(),ecKey, Script.ScriptType.P2PKH));
+        System.out.println(Address.fromKey(DogecoinMainNetParams.get(),ecKey, Script.ScriptType.P2PKH));
     }
 }
